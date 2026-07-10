5e8d9fdf05cc1ff868532b803d7d6f25621ddbc9
Erenkocy6 <eren.kocyigit@mni.thm.de>
2026-07-10 14:25:37 +0200

    Forward HTTP redirect and HTTPS through NIPS

--- ips/entrypoint.sh
+++ ips/entrypoint.sh
@@ -8,14 +8,18 @@ NETWORK_ADDRESS_CIDR=$(ip addr show "${NETWORK_INTERFACE_NAME}" | grep "inet\b" 
 NETWORK_ADDRESS=$(echo "${NETWORK_ADDRESS_CIDR}" | cut -d'/' -f1)
 
 TARGET_ADDRESS=$(getent hosts "${FORWARD_HOST}" | awk '{print $1}')
+FORWARD_HTTP_PORT="${FORWARD_HTTP_PORT:-8080}"
 
 echo "Network address: ${NETWORK_ADDRESS}"
 echo "Forward host: ${FORWARD_HOST}"
-echo "Forward port: ${FORWARD_PORT}"
+echo "Forward HTTP port: ${FORWARD_HTTP_PORT}"
+echo "Forward HTTPS port: ${FORWARD_PORT}"
 echo "Target address: ${TARGET_ADDRESS}"
 
-iptables -t nat -A PREROUTING -p tcp --dport "${FORWARD_PORT}" -j DNAT --to-destination "${TARGET_ADDRESS}:${FORWARD_PORT}"
-iptables -t nat -A POSTROUTING -p tcp -d "${TARGET_ADDRESS}" --dport "${FORWARD_PORT}" -j SNAT --to-source "${NETWORK_ADDRESS}"
+for PORT in "${FORWARD_HTTP_PORT}" "${FORWARD_PORT}"; do
+  iptables -t nat -A PREROUTING -p tcp --dport "${PORT}" -j DNAT --to-destination "${TARGET_ADDRESS}:${PORT}"
+  iptables -t nat -A POSTROUTING -p tcp -d "${TARGET_ADDRESS}" --dport "${PORT}" -j SNAT --to-source "${NETWORK_ADDRESS}"
+done
 
 # redirect all forwarded packets to queue watched by snort
 # (queue-num identifies the queue and has to match the snort "--daq-var queue=X" option)
